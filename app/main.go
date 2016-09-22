620034c32242b5dfa3e07f9d38fb9b1f0acf1877
dovneko <dov.neko@gmail.com>
2016-09-22 22:29:31 +0900

    backend upd

--- app/main.go
+++ app/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 
+	"golang.org/x/net/context"
+
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/user"
 )
 
@@ -31,6 +35,8 @@ func tmplHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	createAccount(ctx, u)
+
 	tpl := template.Must(template.ParseFiles("template/index.html"))
 	tpl.Execute(w, "")
 }
@@ -41,7 +47,29 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "aa")
+
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	if u == nil {
+		http.Redirect(w, r, "/index/", http.StatusFound)
+		return
+	}
+
+	account, exists := getAccount(ctx, u)
+
+	if !exists {
+		http.Redirect(w, r, "/index/", http.StatusFound)
+		return
+	}
+
+	js, list_err := json.Marshal(account)
+	if list_err != nil {
+		http.Error(w, list_err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
@@ -66,4 +94,36 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	url, _ := user.LogoutURL(ctx, "/")
 	http.Redirect(w, r, url, http.StatusFound)
+}
+
+type Account struct {
+	Account string
+	Name    string
+	Groups  []*datastore.Key
+}
+
+func getAccount(c context.Context, user *user.User) (Account, bool) {
+	var e Account
+	key := datastore.NewKey(c, "Accounts", user.Email, 0, nil)
+
+	datastore.Get(c, key, &e)
+
+	return e, e.Account != ""
+}
+
+func createAccount(c context.Context, user *user.User) {
+
+	_, exists := getAccount(c, user)
+	if exists {
+		return
+	}
+
+	key := datastore.NewKey(c, "Accounts", user.Email, 0, nil)
+
+	a := Account{
+		Account: user.Email,
+		Name:    user.ClientID,
+	}
+
+	datastore.Put(c, key, &a)
 }
